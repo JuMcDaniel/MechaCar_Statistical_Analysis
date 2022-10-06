7a7195201e6cf2fafda10894aa69bef83bbca8fc
JuMcDaniel <103297084+JuMcDaniel@users.noreply.github.com>
2022-10-06 10:56:54 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -25,3 +25,14 @@ lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>%
                           variance_PSI = var(PSI),
                           SD_PSI = sd(PSI))
 
+##Deliverable 3
+
+t.test(Suspension_Coil$PSI, mu=1500)
+
+Lot1 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot1')
+Lot2 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot2')
+Lot3 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot3')
+
+t.test(Lot1$PSI, mu=1500)
+t.test(Lot2$PSI, mu=1500)
+t.test(Lot3$PSI, mu=1500)
