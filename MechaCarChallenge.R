eb4247fc398eabfbbd4636764e326ffc739cd738
JuMcDaniel <103297084+JuMcDaniel@users.noreply.github.com>
2022-10-06 09:57:17 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,27 @@
+#Deliverable 1
+
 library(dplyr)
 mpg_car <- read.csv('MechaCar_mpg.csv')
 head(mpg_car)
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_car)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_car))
+
+
+#Deliverable 2
+library(tidyverse)
+Suspension_Coil <- read.csv('Suspension_Coil.csv',check.names = F)
+
+head(Suspension_Coil)
+
+total_summary <- summarize(Suspension_Coil, mean_PSI = mean(PSI),
+                           median_PSI = median(PSI),
+                           variance_PSI = var(PSI),
+                           SD_PSI = sd(PSI))
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>%
+                summarize(mean_PSI = mean(PSI),
+                          median_PSI = median(PSI),
+                          variance_PSI = var(PSI),
+                          SD_PSI = sd(PSI))
+
